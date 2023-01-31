d1bd7f78ced31b7ed0b9ef432c26bc65a921b66c
aadhityakumaran <kumaranpoke@gmail.com>
2023-01-31 12:45:21 +0530

    LoL

---
+++ setup.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+db = mysql.connector.connect(host="localhost", user="root", password="sqlsme")
+cr = db.cursor()
+
+cr.execute(
+    """create database library;
+use library;
+
+create table users(
+reg_no varchar(20) primary key,
+password char(64) not null,
+name varchar(30),
+phone_number char(10),
+mail_id varchar(40),
+book1 varchar(15),
+date1 date,
+book2 varchar(15),
+date2 date,
+book3 varchar(15)
+date3 date);
+
+create table books(
+book_id varchar(15) primary key,
+wished_people varchar(40),
+copies int not null,
+book_name varchar(256),
+author varchar(30),
+category varchar(20),
+hotness int);"""
+)
+
+db.close()
