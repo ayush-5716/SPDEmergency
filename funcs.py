7824bd286aab1a2001334e7b1df4d077461f58c7
aadhityakumaran <kumaranpoke@gmail.com>
2023-01-31 12:29:27 +0530

    LoL

--- funcs.py
+++ funcs.py
@@ -1,2 +1,13 @@
-def nothing():
-    pass
+import mysql.connector
+import hashlib
+import datetime
+
+db = mysql.connector.connect(host="localhost", username="root", password="sqlsme", database="library")
+cr = db.cursor()
+
+def library_status():
+    return 8 < datetime.datetime.now().hour < 20
+
+
+
+db.close()
