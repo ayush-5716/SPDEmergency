ab505c8c2c8cbaf543c7493f29a5e53dcf92b613
Ayush <ayushz5716@gmail.com>
2023-01-31 14:50:17 +0530

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Login(QDialog):
         uic.loadUi("UIPages/login.ui",self)
 
         self.login_button.clicked.connect(self.go_to_homepage)
-
+        
     def go_to_homepage(self):
         home_p = home_page()
         widget.addWidget(home_p)
@@ -16,11 +16,32 @@ class Login(QDialog):
 
 
 
+class profile_page(QDialog):
+    def __init__(self):
+        super(profile_page,self).__init__()
+        uic.loadUi("UIPages/profilepage.ui",self)
 
+        self.backButton.clicked.connect(self.goBack)
+    
+    def goBack(self):
+        widget.removeWidget(widget.currentWidget())
+
+    
 class home_page(QDialog):
     def __init__(self):
         super(home_page,self).__init__()
         uic.loadUi("UIPages/homepage.ui",self)
+
+        self.logOut.clicked.connect(self.goBack)
+        self.profileButton.clicked.connect(self.go_to_profile)
+    
+    def goBack(self):
+        widget.removeWidget(widget.currentWidget())
+
+    def go_to_profile(self):
+        profile_page_var = profile_page()
+        widget.addWidget(profile_page_var)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 app = QApplication(sys.argv)
 widget = QStackedWidget()
