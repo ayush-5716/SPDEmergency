72a4a53bae3ace8e471258c474a8b98f4b57a960
aadhityakumaran <kumaranpoke@gmail.com>
2023-01-31 12:42:29 +0530

    LoL

--- funcs.py
+++ funcs.py
@@ -9,5 +9,9 @@ def library_status():
     return 8 < datetime.datetime.now().hour < 20
 
 
+def time_right_now():
+    return datetime.datetime.now().strftime("%X")
 
+def login_check(username, password):
+    cr.execute()
 db.close()
