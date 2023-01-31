e55a83a67a2487bd5f8e985f7a31bb8da72c8dcc
Ayush <ayushz5716@gmail.com>
2023-01-31 14:11:59 +0530

    login commit

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QPushButton,QDialog,QApplication,QStackedWidget
+from PyQt5 import uic
+import sys
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login,self).__init__()
+        uic.loadUi("UIPages/login.ui",self)
+        
+
+
+app = QApplication(sys.argv)
+widget = QStackedWidget()
+login = Login()
+widget.addWidget(login)
+widget.setFixedHeight(900)
+widget.setFixedWidth(1200)
+widget.show()
+
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exit")
